c709981ad838d5f178ea59a15d8bc57dbf623ef5
Caishengjian_home_pc <1294797029@qq.com>
2020-07-13 22:43:15 +0800

    'init_a'

--- src/main/kotlin/com/example/demo/entity/User.kt
+++ src/main/kotlin/com/example/demo/entity/User.kt
@@ -3,13 +3,12 @@ package com.example.demo.entity
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
-import javax.persistence.Id
-
 
 @Entity
-class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    val id:Long=0,
-    val name:String
+class User (
+        @javax.persistence.Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id:Long=-1,
+        val name:String=""
 )
+
--- src/main/kotlin/com/example/demo/repository/UserRepo.kt
+++ src/main/kotlin/com/example/demo/repository/UserRepo.kt
@@ -2,6 +2,8 @@ package com.example.demo.repository
 
 import com.example.demo.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface UserRepo : JpaRepository<User, Long> {
+    override fun findById(id: Long): Optional<User>
 }
